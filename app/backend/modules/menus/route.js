3f0b76fe9177497cb0522421dd2b111e0ec64a93
QuocCuong <trquoccuong@gmail.com>
2015-06-17 17:53:47 +0700

    update

--- app/backend/modules/menus/route.js
+++ app/backend/modules/menus/route.js
@@ -6,13 +6,13 @@ let controller = require('./controllers/index.js');
 
 let moduleName = 'menu';
 
-router.route('/menu').get(__acl.isAllow(moduleName, 'index'), controller.index);
-router.get('/menu/sort/:sort/:order', __acl.isAllow(moduleName, 'index'), controller.index);
-router.route('/menu').delete(__acl.isAllow(moduleName, 'delete'),controller.delete);
-router.route('/menu/create').get(__acl.isAllow(moduleName, 'create'), controller.create);
-router.route('/menu/create').post(__acl.isAllow(moduleName, 'create'), controller.save);
-router.route('/menu/update/:cid').get(__acl.isAllow(moduleName, 'update'),controller.read);
-router.route('/menu/update/:cid').post(__acl.isAllow(moduleName, 'update'), controller.update);
+router.route('/menus').get(__acl.isAllow(moduleName, 'index'), controller.index);
+router.get('/menus/sort/:sort/:order', __acl.isAllow(moduleName, 'index'), controller.index);
+router.route('/menus').delete(__acl.isAllow(moduleName, 'delete'),controller.delete);
+router.route('/menus/create').get(__acl.isAllow(moduleName, 'create'), controller.create);
+router.route('/menus/create').post(__acl.isAllow(moduleName, 'create'), controller.save);
+router.route('/menus/update/:cid').get(__acl.isAllow(moduleName, 'update'),controller.read);
+router.route('/menus/update/:cid').post(__acl.isAllow(moduleName, 'update'), controller.update);
 router.param('cid', controller.menuById);
 
 module.exports = router;
