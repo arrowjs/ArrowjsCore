22cf304badddce6ad4b352adc3ab4ed5bb88a6a8
QuocCuong <trquoccuong@gmail.com>
2015-09-10 15:42:27 +0700

    fix logic loading database

--- libs/models_manager.js
+++ libs/models_manager.js
@@ -4,9 +4,9 @@ let path = require("path");
 let Sequelize = require("sequelize");
 let sequelize;
 
-var __ =  require('./global_function');
+var __ = require('./global_function');
 
-if(__config.db) {
+if (__config.db) {
     sequelize = new Sequelize(__config.db.database, __config.db.username, __config.db.password, __config.db);
 }
 
@@ -23,12 +23,13 @@ __.getGlobbedFiles(__base + 'app/modules/*/models/*.js').forEach(function (route
     let model = sequelize["import"](path.resolve(routePath));
     db[model.name] = model;
 });
-
 Object.keys(db).forEach(function (modelName) {
     if ("associate" in db[modelName]) {
-        db[modelName].associate(db);
+        db[modelName].associate(db)
     }
 });
+
+sequelize.sync();
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
