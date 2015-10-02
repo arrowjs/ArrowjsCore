e9b774ebc295444393e8c4ab423b434e49c78bd8
QuocCuong <trquoccuong@gmail.com>
2015-10-02 16:13:40 +0700

    auto reset config

--- libs/config_manager.js
+++ libs/config_manager.js
@@ -25,19 +25,7 @@ function init() {
      * We'll Look for a valid NODE_ENV variable and if one cannot be found load the development NODE_ENV
      */
     var conf = {}
-
-    glob(__base + 'config/env/' + process.env.NODE_ENV + '.js', {
-        sync: true
-    }, function (err, environmentFiles) {
-        if (!environmentFiles.length) {
-            if (process.env.NODE_ENV) {
-                console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
-            } else {
-                console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
-            }
-        }
-    });
-
+    console.log(process.env.NODE_ENV);
     if (!fs.existsSync(__base + 'config/env/all.js')) {
         fsEx.copySync(path.resolve(__dirname, '..', 'demo/all.js'), __base + 'config/env/all.js');
         _.assign(conf, require(__base + 'config/env/all.js'));
@@ -51,6 +39,19 @@ function init() {
         fsEx.copySync(path.resolve(__dirname, '..', 'demo/development.js'), __base + 'config/env/' + process.env.NODE_ENV + '.js');
         _.assign(conf, require(__base + 'config/env/' + process.env.NODE_ENV));
     }
+
+    //glob(__base + 'config/env/' + process.env.NODE_ENV + '.js', {
+    //    sync: true
+    //}, function (err, environmentFiles) {
+    //    if (!environmentFiles.length) {
+    //        if (process.env.NODE_ENV) {
+    //            console.error(chalk.red('No configuration file found for "' + process.env.NODE_ENV + '" environment using development instead'));
+    //        } else {
+    //            console.error(chalk.red('NODE_ENV is not defined! Using default development environment'));
+    //        }
+    //    }
+    //});
+
     return conf
 }
 
@@ -64,7 +65,8 @@ module.exports  = getConfig;
 function getConfig() {
     global.__config = init();
     return new Promise(function (fulfill, reject) {
-        redis.del(__config.redis_prefix + __config.key, function () {
+        redis.del(__config.redis_prefix + __config.key, function (err,reply) {
+            console.log(err,reply);
             redis.get(__config.redis_prefix + __config.key, function (err, config) {
                 if (err) reject(err);
                 if (config) {
