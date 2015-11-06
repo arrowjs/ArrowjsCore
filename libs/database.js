06cf9567f021d4d629ed061677d6bdca2e4fb202
QuocCuong <trquoccuong@gmail.com>
2015-11-06 16:12:59 +0700

    model attribute : connect database by sequelize"

--- libs/database.js
+++ libs/database.js
@@ -2,22 +2,15 @@
 
 let Sequelize = require('sequelize');
 
-//TODO : Need wrap database here;
+//TODO : If db.database = mongo call mongoose :
 module.exports = function (application) {
     let sequelize = new Sequelize(application._config.db.database, application._config.db.username, application._config.db.password, application._config.db)
 
-    sequelize.on('error', function (err) {
-        console.log(err);
-        return new Database()
-    })
+    sequelize.query('select').catch(function (err) {
+        console.log("Database connect error : Check your database config in file config/" + process.env.NODE_ENV + ".js");
+        console.log("Database connect error : " + err.message);
+        process.exit();
+    });
     return sequelize;
-}
+};
 
-class Database {
-    import() {
-
-    }
-    associate(){
-
-    }
-}
--- manager/SystemManager.js
+++ manager/SystemManager.js
@@ -121,7 +121,7 @@ class SystemManager extends events.EventEmitter {
                 components[name]._structure = struc.path[id] || struc;
                 components[name].controllers = {};
                 components[name].routes = Express.Router();
-                //components[name].models = {};
+                components[name].models = {};
                 //components[name].helpers = {};
                 //components[name].views = [];
 
@@ -223,19 +223,38 @@ function extendsAttribute(setting) {
 }
 
 function modelAttribute(setting, fatherPath, component, application) {
-    let files = getlistFile(setting, fatherPath, application.arrFolder);
+    let files = getlistFile(setting, fatherPath, application);
     let database = Database(application);
-    let db = {};
-    Object.keys(files).map(function (key) {
-        let model = database.import(path.resolve(files[key]));
-        db[model.name] = model
-    });
-    Object.keys(db).forEach(function (modelName) {
-        if ("associate" in db[modelName]) {
-            db[modelName].associate(db)
-        }
-    });
-    component.models = db;
+    if (files.type === "single") {
+        Object.keys(files).map(function (key) {
+            if (key !== "type") {
+                files[key].map(function (link) {
+                    let model = database.import(path.resolve(link));
+                    component.models[model.name] = model
+                })
+            }
+        })
+        Object.keys(component.models).forEach(function (modelName) {
+            if ("associate" in component.models[modelName]) {
+                component.models[modelName].associate(component.models)
+            }
+        });
+    } else if (files.type === "multi") {
+        Object.keys(files).map(function (key) {
+            if (key !== "type") {
+                component.models[key] = {};
+                files[key].map(function (link) {
+                    let model = database.import(path.resolve(link));
+                    component.models[key][model.name] = model
+                })
+                Object.keys(component.models[key]).forEach(function (modelName) {
+                    if ("associate" in component.models[key][modelName]) {
+                        component.models[key][modelName].associate(component.models[key])
+                    }
+                });
+            }
+        })
+    }
 }
 function viewAttribute(setting, fatherPath, component, application) {
     let obj = Object.create(null);
