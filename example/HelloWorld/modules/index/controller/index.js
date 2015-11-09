cb6eaae661228ea1f8fa5dcbd568853d481b7a09
techmaster <cuong@techmaster.vn>
2015-11-09 10:37:51 +0700

    Add more features to HelloWorld app

--- example/HelloWorld/modules/index/controller/index.js
+++ example/HelloWorld/modules/index/controller/index.js
@@ -2,10 +2,33 @@
 
 module.exports = function (controller,component,application) {
     controller.index = function (req,res) {
+
         res.send("Welcome to Arrowjs.io<br><a href='\about'>About us</a>");
     };
-
+    /**
+     * Return view using Nunjuck template
+     * @param req
+     * @param res
+     */
     controller.about = function (req,res) {
-        res.send("Written by TechMaster and you");
+        res.render('about',
+            {
+                title: 'About Arrowjs.io',
+                body: 'Arrowjs.io is framework that is fast, theme-able and extensible'
+            })
     };
+    /**
+     * return JSON
+     * @param req
+     * @param res
+     */
+    controller.repos = function(req, res) {
+        let repos = [
+            { name: 'arrowjs core', url: 'https://github.com/arrowjs/ArrowjsCore' }
+            , { name: 'arrowjs cms', url: 'https://github.com/arrowjs/ArrowCMS' }
+            , { name: 'examples', url: 'https://github.com/arrowjs/examples' }
+            , { name: 'documents', url: 'https://github.com/arrowjs/Documents' }
+        ];
+        res.json(repos);
+    }
 };
--- example/HelloWorld/modules/index/route.js
+++ example/HelloWorld/modules/index/route.js
@@ -4,4 +4,6 @@ module.exports = function (route,component,application) {
     route.route("/").get(component.controllers.index);
 
     route.route("/about").get(component.controllers.about);
+
+    route.route("/repos").get(component.controllers.repos);
 };
