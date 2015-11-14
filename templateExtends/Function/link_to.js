b9483c0f960a602e6281d67c38977acc38fb4689
vhchung <chung.k54thb@gmail.com>
2015-11-14 15:04:03 +0700

    - Fix error when call function link_to with no param #14

--- templateExtends/Function/link_to.js
+++ templateExtends/Function/link_to.js
@@ -4,13 +4,15 @@ module.exports = {
     name : 'link_to',
     handler : function (name,option) {
         let route = this._arrRoutes[name];
-        Object.keys(option).map(function (key) {
-            let newKey = key;
-            if (key[0] !== ":") {
-                newKey = ":" + key;
-            }
-            route = route.replace(newKey,option[key]);
-        });
+        if(option) {
+            Object.keys(option).map(function (key) {
+                let newKey = key;
+                if (key[0] !== ":") {
+                    newKey = ":" + key;
+                }
+                route = route.replace(newKey,option[key]);
+            });
+        }
         return route;
     }
-}
+};
