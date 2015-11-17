fe5431a43f4468c2b21a9bdd41712c6ec6a96ad2
QuocCuong <trquoccuong@gmail.com>
2015-11-17 10:20:58 +0700

    fix logic flash message

--- libs/flashMessage.js
+++ libs/flashMessage.js
@@ -4,38 +4,29 @@ let _ = require('lodash');
 
 module.exports = function () {
     let app = this;
-//TODO : testing flash messages;
     app.use(function (req, res, next) {
-        res.locals.messages = req.session.messages;
+        res.locals.messages = req.session.flash;
 
-        if (!req.session.messages) {
-            req.session.messages = [];
+        if (!req.session.flash) {
+            req.session.flash = [];
         }
 
         let easyFlash = {
             success: function (content) {
-                req.session.messages.push({
-                    type: 'success',
-                    content: content
-                })
+                req.session.flash.success = req.session.flash.success || [];
+                req.session.flash.success.push(content)
             },
             error: function (content) {
-                req.session.messages.push({
-                    type: 'error',
-                    content: content
-                })
+                req.session.flash.error = req.session.flash.error || [];
+                req.session.flash.error.push(content)
             },
             warn: function (content) {
-                req.session.messages.push({
-                    type: 'warn',
-                    content: content
-                })
+                req.session.flash.warn = req.session.flash.warn || [];
+                req.session.flash.warn.push(content)
             },
             info: function (content) {
-                req.session.messages.push({
-                    type: 'info',
-                    content: content
-                })
+                req.session.flash.info = req.session.flash.info || [];
+                req.session.flash.info.push(content)
             }
         };
         _.assign(req.flash, easyFlash);
