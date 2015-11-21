377da87968512de89143d0a211ae457be49334ac
Tran Quoc Cuong <trquoccuong@gmail.com>
2015-11-21 11:09:12 +0700

    minimize config services example

--- example/ZeroMQEx/B/config/services.js
+++ example/ZeroMQEx/B/config/services.js
@@ -1,20 +1,7 @@
 "use strict";
 
 module.exports = {
-    //services : {
-    //    user : {
-    //        protocol : "tcp",
-    //        host : "127.0.0.1",
-    //        port : "3333",
-    //        type : "req",
-    //    }
-    //},
     service_setting: {
-        enable : true,
-        //protocol : "tcp",
-        //host : "127.0.0.1",
-        //port : "3333",
-        //type : "pub",
-        //connect_type : "connect" // "bindSync" , "bind"
+        enable : true
     }
 };
