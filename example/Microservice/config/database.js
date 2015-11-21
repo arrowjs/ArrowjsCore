06818be88d0bd788cc681de87687063a8de4bbe0
techmaster <cuong@techmaster.vn>
2015-11-21 18:21:02 +0700

    Move micro service example to repo Examples

--- example/Microservice/config/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-
-module.exports = {
-    associate : function (models) {
-
-    }
-};
--- example/ZeroMQEx/A/server.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const Arrow = require('arrowjs');
-const arrowZmq = require('arrow-zeromq');
-
-const application = new Arrow();
-application.setConfig("port","3333");
-application.addPlugin(arrowZmq());
-application.start();
