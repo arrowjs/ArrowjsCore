b3add264818689e23d3ae6d13f0374c229d4ca15
Tran Quoc Cuong <trquoccuong@gmail.com>
2015-11-21 11:09:12 +0700

    minimize config services example

--- example/ZeroMQEx/A/config/services.js
+++ example/ZeroMQEx/A/config/services.js
@@ -6,25 +6,6 @@ module.exports = {
             protocol : "tcp",
             host : "127.0.0.1",
             port : "5555",
-            //type : "sub",
-            //subscribe : "key",
-            //monitor_events : {
-            //    connect : function (fd, ep) {
-            //        console.log('connect, endpoint:', ep);
-            //    }
-            //},
-            //monitor : {
-            //    interval :500,
-            //    numOfEvents : 0
-            //}
         }
-    },
-    //service_setting: {
-    //    enable : true,
-    //    protocol : "tcp",
-    //    host : "127.0.0.1",
-    //    port : "3333",
-    //    type : "req",
-    //    connect_type : "connect" // "bindSync" , "bind"
-    //}
+    }
 };
