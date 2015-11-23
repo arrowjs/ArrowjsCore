d0f84d94c4c159ea091a26e1fea5a96696b2fc51
Tran Quoc Cuong <trquoccuong@gmail.com>
2015-11-23 21:32:52 +0700

    remove all example

--- example/Logger/server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const Arrow = require('../..');
-const express = require('express');
-
-const application = new Arrow();
-application.use(express.static(__dirname + '/public/themes/freelancer'));
-application.start();
--- example/ThemeSupport/server.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const Arrow = require('../..');
-const application = new Arrow();
-
-
-application.start();  //listen on port 8000, to change port go to /config/env/development.js change port number
