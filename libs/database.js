96224e954e3795096d698ee9881f07961d93f66a
QuocCuong <trquoccuong@gmail.com>
2015-11-30 14:36:44 +0700

    Support mysql and sqlite

--- libs/database.js
+++ libs/database.js
@@ -9,7 +9,7 @@ module.exports = function (application) {
     db = db || new Sequelize(application._config.db.database, application._config.db.username, application._config.db.password, application._config.db)
 
     db.query('select').catch(function (err) {
-        logger.error("Database connect error : Check your database config in file config/" + process.env.NODE_ENV + ".js");
+        logger.error("Database connect error : Check your database config in file config/env/" + process.env.NODE_ENV + ".js");
         logger.error("Database connect error : " + err.message);
         process.exit();
     });
